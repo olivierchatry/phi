edaacd84968b292f24663baf019090db1859dceb
Olivier Chatry <olivier.chatry@gmail.com>
2014-11-27 12:05:30 +0100

    x linux build

--- premake5.lua
+++ premake5.lua
@@ -1,56 +1,4 @@
---
--- Allows copying directories.
--- It uses the premake4 patterns (**=recursive match, *=file match)
--- NOTE: It won't copy empty directories!
--- Example: we have a file: src/test.h
---	os.copydir("src", "include") simple copy, makes include/test.h
---	os.copydir("src", "include", "*.h") makes include/test.h
---	os.copydir(".", "include", "src/*.h") makes include/src/test.h
---	os.copydir(".", "include", "**.h") makes include/src/test.h
---	os.copydir(".", "include", "**.h", true) will force it to include dir, makes include/test.h
---
--- @param src_dir
---    Source directory, which will be copied to dst_dir.
--- @param dst_dir
---    Destination directory.
--- @param filter
---    Optional, defaults to "**". Only filter matches will be copied. It can contain **(recursive) and *(filename).
--- @param single_dst_dir
---    Optional, defaults to false. Allows putting all files to dst_dir without subdirectories.
---    Only useful with recursive (**) filter.
--- @returns
---    True if successful, otherwise nil.
---
-function os.copydir(src_dir, dst_dir, filter, single_dst_dir)
-	if not os.isdir(src_dir) then error(src_dir .. " is not an existing directory!") end
-	filter = filter or "**"
-	src_dir = src_dir .. "/"
-	print('copy "' .. src_dir .. filter .. '" to "' .. dst_dir .. '".')
-	dst_dir = dst_dir .. "/"
-	local dir = path.rebase(".",path.getabsolute("."), src_dir) -- root dir, relative from src_dir
- 
-	os.chdir( src_dir ) -- change current directory to src_dir
-		local matches = os.matchfiles(filter)
-	os.chdir( dir ) -- change current directory back to root
- 
-	local counter = 0
-	for k, v in ipairs(matches) do
-		local target = iif(single_dst_dir, path.getname(v), v)
-		--make sure, that directory exists or os.copyfile() fails
-		os.mkdir( path.getdirectory(dst_dir .. target))
-		if os.copyfile( src_dir .. v, dst_dir .. target) then
-			counter = counter + 1
-		end
-	end
- 
-	if counter == #matches then
-		print( counter .. " files copied.")
-		return true
-	else
-		print( "Error: " .. counter .. "/" .. #matches .. " files copied.")
-		return nil
-	end
-end
+dofile "premake-utils.lua"
 
 solution "Phi"
 	configurations {"Debug", "Release"}
@@ -71,24 +19,24 @@ project "Phi"
 	location ("build")
 	floatingpoint "Fast"
 	flags       { "Symbols" }
-  debugdir "Phi"
+	debugdir "Phi"
 
-  os.mkdir("build")
-  os.copydir("Phi/Shaders", "build/Shaders")
-  defines { "GLEW_STATIC", "GLM_FORCE_RADIANS" }
+	os.mkdir("build")
+	os.copydir("Phi/Shaders", "build/Shaders")
+	defines { "GLEW_STATIC", "GLM_FORCE_RADIANS" }
 
-  configuration { "**.c"}
-    flags { "NoPCH" }
-    
+	configuration { "**.c"}
+		flags { "NoPCH" }
+
 	configuration "vs*"
 		pchheader "PreCompile.h"
 		pchsource "Phi/PreCompile.cpp"
 		defines     { "_CRT_SECURE_NO_WARNINGS" }
-		
+
 	configuration "vs2005"
 		defines        {"_CRT_SECURE_NO_DEPRECATE" }
 	
-  configuration "windows"  
+	configuration "windows"  
 		os.copyfile("Externals/glfw/lib/glfw3.dll", "build/glfw3.dll")
 		libdirs "Externals/glfw/lib"
 		system "Windows"
@@ -99,14 +47,24 @@ project "Phi"
 		buildoptions { "-mmacosx-version-min=10.4" }
 		linkoptions  { "-mmacosx-version-min=10.4" }
 
-  configuration "macosx"
+	configuration "macosx"
 		libdirs "Externals/glfw/lib/osx"
 		links       { "glfw3", "OpenGL.framework", "CoreFoundation.framework", "Cocoa.framework", "IOKit.framework", "AGL.framework"}
-	
+	configuration "linux"
+		buildoptions { "-std=c++11" }
+		linkoptions  { "-std=c++11" }
+
+		if os.is64bit() then
+			libdirs "Externals/glfw/lib/linux/x86_64"
+		else
+			libdirs "Externals/glfw/lib/linux/x86"
+		end
+		links       { "glfw3", "X11", "Xxf86vm", "Xrandr", "Xi", "GL", "pthread"}
+		
 	configuration "Debug"
 		defines { "DEBUG" }
 		optimize "Debug"
-		
+
 	configuration "Release"
-		defines { "NDEBUG" }
-		optimize "Full"
+	defines { "NDEBUG" }
+	optimize "Full"
